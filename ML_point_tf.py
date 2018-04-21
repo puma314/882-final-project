296cf85a4ee240a21026e00fddd345258b606865
jlin816 <lnj@mit.edu>
2018-04-21 18:12:01 -0400

    Rewrote everything lmao

--- ML_point_tf.py
+++ ML_point_tf.py
@@ -1,68 +1,94 @@
 import tensorflow as tf
 import numpy as np
 
-alpha = 1e-3 #task learning rate
-beta = 0.001 #meta learning rate
-K = 1 #number of gradient updates in task training
-N = 10*2 #number of samples used for task training
-M = 10*2 #number of samples used for task testing
-J = 25 #number of different tasks to train on in each iteration
-meta_training_iters = 70000
+# MAML parameters
+alpha = 1e-3        # task learning rate
+beta = 0.001        # meta learning rate
+K = 1               # number of gradient updates in task training
+N = 10*2            # number of samples used for task training
+M = 10*2            # number of samples used for task testing
+J = 25              # number of different tasks to train on in each iteration
+meta_training_iters = 10
 
-def sample_sin_task(N, task):
-    # Sample observations from task
-    input_points = tf.random_uniform((N,), minval=-10, maxval=10)
-    output_points = tf.scalar_mul(task['amplitude'], tf.sin(input_points + task['phase']))
+# Network parameters
+n_fc = 40
+
+class MAML_HB():
+    def __init__(self):
+        self.theta = {
+            "w1": tf.Variable(tf.random_normal([N, n_fc])),
+            "b1": tf.Variable(tf.zeros([n_fc])),
+            "w2": tf.Variable(tf.random_normal([n_fc, n_fc])),
+            "b2": tf.Variable(tf.zeros([n_fc])),
+            "out": tf.Variable(tf.random_normal([n_fc, N]))
+        }
+
+        self.tasks = [tf.placeholder(tf.float32, shape=(2,)) for _ in range(J)]
+        self.train_op, self.loss = self.build_train_op()
+    
+    def ML_point(self, task):
+        amplitude, phase = tf.unstack(task)
+        input_pts, output_pts = sample_sin_task_pts(N, amplitude, phase)
+
+        phi = {}
+        for key, val in self.theta.items():
+            phi[key] = tf.Variable(val.initialized_value())
+
+        import pdb; pdb.set_trace()
+
+        for k in range(K):
+            pred = self.forward_pass(input_pts, phi)
+            loss = mse(pred, output_pts) # higher loss means lower negative logprob
+            grad = tf.gradients(loss, list(phi.values()))
+            grad = dict(zip(phi.keys(), grad))
+
+            for key, val in phi.items():
+                tf.assign(val, val - alpha * grad[key])
+    
+        test_input_pts, test_output_pts = sample_sin_task_pts(M, amplitude, phase)
+        test_pred = self.forward_pass(test_input_pts, phi)
+        return mse(test_pred, test_output_pts)
+
+    def forward_pass(self, inp, params):
+        fc1 = tf.add(tf.matmul(inp, params["w1"]), params["b1"])
+        fc1 = tf.nn.relu(fc1)
+        fc2 = tf.add(tf.matmul(fc1, params["w2"]), params["b2"])
+        fc2 = tf.nn.relu(fc2)
+        out = tf.matmul(fc2, params["out"]) + params["out"]
+        return out
+
+    def build_train_op(self):
+        " One iter of the outer loop. "
+        task_losses = []
+        for task in self.tasks:
+            task_loss = self.ML_point(task)
+            task_losses.append(task_loss)
+        import pdb; pdb.set_trace()
+        loss = tf.add_n(task_losses)
+        print("Loss shape:", task_losses)
+        grad = tf.gradients(loss, list(self.theta.values()))
+        grad = dict(zip(self.theta.keys(), grad))
+        return tf.group([tf.assign(val, val - beta * grad[key]) for key, val in self.theta.items()]), loss
+
+
+def draw_sin_tasks(J):
+    " Returns a set of sampled sin tasks (amplitude, phase). "
+    return [
+        np.array(
+            np.random.uniform(minval=0.1, maxval=5.0),
+            np.random.uniform(minval=0.0, maxval=np.pi)
+        ) 
+        for _ in range(J)
+    ]
+
+def sample_sin_task_pts(N, amplitude, phase):
+    " Given sin task params (amplitude, phase), returns N observations sampled from the task. "
+    input_points = tf.random_uniform((1, N), minval=-10., maxval=10.)
+    output_points = amplitude * tf.sin(input_points + phase)
     return input_points, output_points
 
 def mse(pred, actual):
     return tf.reduce_mean(tf.squared_difference(pred, actual)) 
-
-def ML_point(theta, task, train=False):
-    input_pts, output_pts = sample_sin_task(N, task)
-    import pdb; pdb.set_trace()
-    phi = {
-        "w1": tf.Variable(theta["w1"].initialized_value()),
-        "b1": tf.Variable(theta["b1"].initialized_value())
-    }
-    for k in range(K):
-        pred = toy_model(input_pts, phi) 
-        grad = dict(zip(phi.keys(), tf.gradients(mse(pred, output_pts), phi.values())))
-        phi = dict(zip(phi.keys(), [phi[key] - alpha * grad[key] for key in phi.keys()]))
-    
-    if train:
-        return phi
-
-    test_input, test_output = sample_sin_task(M, task)
-    test_pred = toy_model(test_input, phi)
-    return mse(test_pred, test_output)  
-
-def MAML_HB():
-    theta = {
-        "w1": tf.Variable(tf.random_normal([40, 1])),
-        "b1": tf.Variable(tf.zeros([40]))
-    }
-
-    curr_theta = {
-        "w1": tf.Variable(theta["w1"].initialized_value()),
-        "b1": tf.Variable(theta["b1"].initialized_value())
-    }
-
-    for i in range(meta_training_iters):
-        amplitudes = tf.random_uniform((J,), minval=0.1, maxval=5.0)
-        phases = tf.random_uniform((J,), minval=0.0, maxval=np.pi)
-        for j in range(J):
-            task = {'amplitude': amplitudes[j], 'phase': phases[j]}
-            grad = dict(zip(curr_theta.keys(), tf.gradients(ML_point(curr_theta, task), curr_theta.values())))
-            # Update each param in theta
-            curr_theta = dict(zip(curr_theta.keys(), [curr_theta[key] - beta * grad[key] for key in curr_theta.keys()]))
-        theta = curr_theta
-        print("Iter {}: mse={}".format(i, eval_theta(theta)))
-
-def toy_model(inp, weights):
-    print("inp:", inp)
-    print(inp.shape)
-    return tf.matmul(weights["w1"], inp) + weights["b1"]
 
 def eval_theta(theta):
     task = {"amplitude": 2, "phase": np.pi/2}
@@ -74,10 +100,16 @@ def eval_theta(theta):
     
 def main():
     sess = tf.InteractiveSession()
-    
+    maml = MAML_HB()
     tf.global_variables_initializer().run()
+    for i in range(meta_training_iters):
+        tasks = draw_sin_tasks(J)
+        _, loss = sess.run([maml.train_op, maml.loss], feed_dict={tp: task for tp, task in zip(maml.tasks, tasks)})
+            
+    graph = tf.get_default_graph()
+    writer = tf.summary.FileWriter("logs")
+    writer.add_graph(graph=graph)
 
-    maml = MAML_HB()   
 
 if __name__ == "__main__":
     main()
